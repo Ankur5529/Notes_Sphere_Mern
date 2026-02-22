ba5eb9aa96642d2817a4186376de4b5e7f500435
Ankur5529 <krankur5529@gmail.com>
2026-02-22 17:21:08 +0530

    Fix error: Change generic href=

--- client/src/Components/Footer.js
+++ client/src/Components/Footer.js
@@ -1,17 +1,17 @@
 import "./Footer.css"
 
-export default function Footer(){
-    return(
+export default function Footer() {
+    return (
         <footer className="footer">
             <div className="footer-content">
                 <h3>📘 Notes Sphere</h3>
                 <p>Your personal space to store and organize notes.</p>
 
                 <div className="footer-links">
-                    <a href="#">About</a>
-                    <a href="#">Support</a>
-                    <a href="#">Contact</a>
-                    <a href="#">Privacy</a>
+                    <a href="/#">About</a>
+                    <a href="/#">Support</a>
+                    <a href="/#">Contact</a>
+                    <a href="/#">Privacy</a>
                 </div>
 
                 <p className="footer-copy">© 2026 Notes Sphere. All rights reserved.</p>
